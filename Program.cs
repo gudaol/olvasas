28fa7b6f31a94a10a1f0d37c36ac7d718a53340d
Guba Dániel Olivér <gudaol@ukatemi.com>
2019-10-26 08:36:55 +0200

    Talanjo

--- Program.cs
+++ Program.cs
@@ -10,7 +10,6 @@ namespace olvasas
 
         static string szotagol(string szo)
         {
-            //System.Console.WriteLine(szo);
             string ret = szo;
             List<int> magindex = new List<int>();
             for (int i = 0; i < szo.Length; i++)
@@ -18,29 +17,24 @@ namespace olvasas
                 char kar = szo[i];
                 if (DB.Maganhangzok.Contains(kar))
                 {
-                    //System.Console.WriteLine(i);
                     magindex.Add(i);
                 }
             }
             if (magindex.Count > 1)
             {
-                System.Console.WriteLine(ret);
-                ret = szo[0..(magindex[0] + 1)];
+                ret = szo[0..(magindex[0])];
                 for (int i = 0; i < magindex.Count - 1; i++)
                 {
                     if (magindex[i] + 1 == magindex[i + 1])
                     {
-                        ret += szo[(magindex[i] + 1)..(magindex[i + 1])] + "-" + szo[magindex[i + 1]];
-                        System.Console.WriteLine(ret);
+                        ret += szo[(magindex[i])..(magindex[i + 1])] + "-";
                     }
                     else
                     {
-                        ret += szo[(magindex[i] + 1)..(magindex[i + 1] - 1)] + "-" + szo[magindex[i + 1] - 1];
-                        System.Console.WriteLine(ret);
+                        ret += szo[(magindex[i])..(magindex[i + 1] - 1)] + "-" + szo[magindex[i + 1] - 1];
                     }
                 }
                 ret += szo.Substring(magindex[magindex.Count - 1]);
-                System.Console.WriteLine(ret);
             }
             return ret;
         }
@@ -60,20 +54,30 @@ namespace olvasas
                 System.Console.WriteLine("Add meg számmal, legfeljebb milyen hosszú szavakat keressek!");
                 var hossz = int.Parse(Console.ReadLine());
                 var szurtSzavak = DB.Szavak.Where(p => p.Length <= hossz).Where(p => p.ToLower().All(c => betuk.Contains(c))).Where(p => p.Any(c => DB.Maganhangzok.Contains(c))).ToList();
-                int i = 0;
-                using (var o = new StreamWriter(new FileStream("szavak.txt", FileMode.Create, FileAccess.Write, FileShare.Read)))
-                    foreach (var szo in szurtSzavak)
-                    {
-                        string v = szo + " :: " + szotagol(szo) + ' ';
-                        System.Console.Write(v);
-                        o.Write(v.AsSpan());
-                        if ((i++ % 10) == 9)
-                        {
-                            o.Write(Environment.NewLine.AsSpan());
-                            System.Console.Write(Environment.NewLine);
-                        }
+                var szavak = new List<string>();
+                int j = 0;
+                var r = new Random();
+                while (j++ < 30 || szurtSzavak.Count == 0)
+                {
+                    var selected = r.Next(szurtSzavak.Count);
+                    szavak.Add(szurtSzavak[selected]);
+                    szurtSzavak.RemoveAt(selected);
+                }
 
+                int i = 0;
+                using var o = new StreamWriter(new FileStream("szavak.txt", FileMode.Create, FileAccess.Write, FileShare.Read));
+                foreach (var szo in szavak)
+                {
+                    string v = szotagol(szo) + ' ';
+                    System.Console.Write(v);
+                    o.Write(v);
+                    if ((i++ % 5) == 4)
+                    {
+                        o.Write(Environment.NewLine);
+                        System.Console.Write(Environment.NewLine);
                     }
+
+                }
             }
             catch (System.Exception e)
             {
